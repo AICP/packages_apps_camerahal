77d8fecddf6d1fb3507601eea13fc7024fe7c3d6
techyguyperplexable <objecting@objecting.org>
2025-12-23 05:26:35 +0530

    CameraHALChecker: save logcat to Downloads

--- app/src/main/java/com/lineageos/camerahalcheck/RootLogcat.kt
+++ app/src/main/java/com/lineageos/camerahalcheck/RootLogcat.kt
@@ -1,28 +1,45 @@
 package com.lineageos.camerahalcheck
 
+import android.os.Build
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 object RootLogcat {
 
     fun captureAll(): File? {
-        return try {
-            val out = File(
-                "/sdcard/Download/CameraHALChecker",
-                "logcat_${System.currentTimeMillis()}.txt"
+        val baseDir = File("/storage/emulated/0/Download/CameraHALChecker")
+        if (!baseDir.exists()) baseDir.mkdirs()
+
+        val ts = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(Date())
+        val outFile = File(baseDir, "logcat_all_$ts.txt")
+
+        val cmd = if (hasRoot()) {
+            arrayOf(
+                "su", "-c",
+                "logcat -b all -d -v threadtime > ${outFile.absolutePath}"
             )
-            out.parentFile?.mkdirs()
+        } else {
+            arrayOf(
+                "sh", "-c",
+                "logcat -b all -d -v threadtime > ${outFile.absolutePath}"
+            )
+        }
 
-            Runtime.getRuntime().exec(
-                arrayOf(
-                    "su",
-                    "-c",
-                    "logcat -b all -d > ${out.absolutePath}"
-                )
-            ).waitFor()
-
-            out
+        return try {
+            Runtime.getRuntime().exec(cmd).waitFor()
+            outFile
         } catch (t: Throwable) {
             null
+        }
+    }
+
+    private fun hasRoot(): Boolean {
+        return try {
+            Runtime.getRuntime().exec(arrayOf("su", "-c", "id")).waitFor() == 0
+        } catch (_: Throwable) {
+            false
         }
     }
 }
